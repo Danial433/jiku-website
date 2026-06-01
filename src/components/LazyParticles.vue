e4dfb3958d03315bdaef4757765e58f0618cadac
Danial <danialaqil433@gmail.com>
2026-06-01 23:33:51 +0200

    particles too big

---
+++ src/components/LazyParticles.vue
@@ -0,0 +1,33 @@
+<template>
+  <component
+    :is="ParticlesComponent"
+    v-if="ParticlesComponent"
+    id="tsparticles"
+    :options="options"
+    class="fixed inset-0 -z-10"
+  />
+</template>
+
+<script setup>
+import { shallowRef, onMounted } from "vue";
+
+defineProps({
+  options: {
+    type: Object,
+    required: true
+  }
+});
+
+const ParticlesComponent = shallowRef(null);
+
+onMounted(async () => {
+  const [{ default: Particles }, { loadSlim }] = await Promise.all([
+    import("@tsparticles/vue3"),
+    import("@tsparticles/slim")
+  ]);
+
+  ParticlesComponent.value = Particles;
+
+  // Note: if this does not initialize correctly, we’ll adjust based on your current particle component.
+});
+</script>
--- src/main.js
+++ src/main.js
@@ -3,35 +3,18 @@ import "./assets/main.css";
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import { MotionPlugin } from "@vueuse/motion";
-
-import Particles from "@tsparticles/vue3";
-import { loadSlim } from "@tsparticles/slim";
+import { Icon } from "@iconify/vue";
 
 import App from "./App.vue";
 import router from "./router";
 
-import { Icon, addCollection } from "@iconify/vue";
-import mingcuteIcons from "@iconify-json/mingcute/icons.json";
-import deviconIcons from "@iconify-json/devicon/icons.json";
-import logosIcons from "@iconify-json/logos/icons.json";
-
 import Separator from "./components/Separator.vue";
-
-addCollection(mingcuteIcons);
-addCollection(deviconIcons);
-addCollection(logosIcons);
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
 app.use(MotionPlugin);
-
-Particles(app, {
-  init: async (engine) => {
-    await loadSlim(engine);
-  },
-});
 
 app.component("Separator", Separator);
 app.component("Icon", Icon);
